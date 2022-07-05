8b284edf1809da6ef65014caf5fd56432bf6862f
tychique <tychmbaz@gmail.com>
2022-07-05 17:14:40 +0800

    mon projet tych

--- app/src/main/java/org/esissalama/mobile/demo/MainActivity.java
+++ app/src/main/java/org/esissalama/mobile/demo/MainActivity.java
@@ -3,12 +3,38 @@ package org.esissalama.mobile.demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Integer[] tab = {
+            R.drawable.dice_1,
+            R.drawable.dice_2,
+            R.drawable.dice_3,
+            R.drawable.dice_4,
+            R.drawable.dice_5,
+            R.drawable.dice_6
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bouton = findViewById(R.id.btn1);
+        Random random = new Random();
+        ImageView img = findViewById(R.id.b1);
+
+        bouton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                img.setImageResource(tab[(random.nextInt(tab.length))]);
+            }
+        });
     }
 }
